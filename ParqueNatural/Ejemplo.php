7024754d8658c76cb5bbbc512cea5a3fb3a884b8
PedroGonzalezMarcos <not2pedro@gmail.com>
2023-05-18 19:27:08 +0200

    JEJEJE

--- ParqueNatural/Ejemplo.php
+++ ParqueNatural/Ejemplo.php
@@ -3,9 +3,9 @@ include('Persona.php');
 include('Animal.php');
 //creo una Persona.
 $persona_1 = new Persona("Pepe", 40);
-echo "Creamos a Persona llamada " . $persona_1->getName() . "\n";
+echo "La persona se llama " . $persona_1->getName() . "\n";
 
 //Creo un animal y le añado a Persona.
 $animal_1 = new Animal('Linda', 5, $persona_1);
-echo "Creamos un animal llamado " . $animal_1->getName() . " propiedad de "
+echo "El animal se llama " . $animal_1->getName() . " propiedad de "
      . $animal_1->getPepHolder()->getName() . "\n";
